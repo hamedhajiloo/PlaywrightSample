e78788788297c21ea8b1402b81de5224930ef5c6
Hamed Hajiloo <45947371+hamedhajiloo@users.noreply.github.com>
2022-11-12 11:31:01 +0330

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Playwright;
+
+var token = "Token";
+using var playwright = await Playwright.CreateAsync();
+await using var browser = await playwright.Chromium.LaunchAsync();
+var page = await browser.NewPageAsync();
+await page.GotoAsync("https://playwright.dev/dotnet");
+
+var a = await page.EvaluateAsync("localStorage.setItem('accept-contact-terms','true');");
+
+await page.Context.AddCookiesAsync(new List<Microsoft.Playwright.Cookie> {
+    new Microsoft.Playwright.Cookie
+    {
+        Name= "token",
+        Value= token,
+        Domain = ".divar.ir",
+        Path="/",
+    }
+});
+
+var getContactElement = page.Locator(".post-actions__get-contact");
+
+await getContactElement.ClickAsync();
+
+await page.ScreenshotAsync(new()
+{
+    Path = "c:\\screenshot.png"
+});
